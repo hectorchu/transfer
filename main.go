4b46bfa5abb06cb15811f1bf1f41d440b90eda3a
Hector Chu <hectorchu@gmail.com>
2020-05-17 21:53:26 +0100

    Better error handling

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"hash/crc32"
 	"io"
 	"net"
 	"os"
+	"time"
 
 	"github.com/cheggaaa/pb/v3"
 )
@@ -25,27 +27,49 @@ func main() {
 		if len(os.Args) < 3 {
 			exit("Needs filename argument")
 		}
-		listen(os.Args[2])
+		if err := listen(os.Args[2]); err != nil {
+			exit(err.Error())
+		}
 	} else {
-		connect(os.Args[1])
+		for {
+			if err := connect(os.Args[1]); err != nil {
+				fmt.Println("Error:", err)
+				fmt.Println("Reconnecting...")
+				time.Sleep(2 * time.Second)
+				continue
+			}
+			break
+		}
 	}
 }
 
 const port = ":3333"
 const chunkSize = 64 * 1024
 
-func listen(filename string) {
+func listen(filename string) (err error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
 	fmt.Println("Serving", filename)
+	fmt.Print("Calculating hashes...")
+	hashes, err := calcHashes(file)
+	file.Close()
+	if err != nil {
+		return
+	}
+	fmt.Println(" done")
 	ln, err := net.Listen("tcp", port)
 	if err != nil {
-		exit("Failed to listen")
+		return
 	}
+	defer ln.Close()
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			exit("Failed to accept")
+			return err
 		}
-		go handleConnection(conn, filename)
+		go handleConnection(conn, filename, hashes)
 	}
 }
 
@@ -56,98 +80,123 @@ func calcChunkSize(chunkIndex int, fileSize int64) int64 {
 	return chunkSize
 }
 
-func calcHashes(file *os.File) (hashes []uint32) {
-	stat, _ := file.Stat()
+func calcHashes(file *os.File) (hashes []uint32, err error) {
+	stat, err := file.Stat()
+	if err != nil {
+		return
+	}
 	size := stat.Size()
-	file.Seek(0, 0)
+	if _, err = file.Seek(0, 0); err != nil {
+		return
+	}
 	chunks := (size + chunkSize - 1) / chunkSize
-	hashes = make([]uint32, chunks, chunks)
+	hashes = make([]uint32, chunks)
 	for i := range hashes {
 		h := crc32.NewIEEE()
-		if _, err := io.CopyN(h, file, calcChunkSize(i, size)); err != nil {
-			exit("Failed to hash file")
+		if _, err = io.CopyN(h, file, calcChunkSize(i, size)); err != nil {
+			return
 		}
 		hashes[i] = h.Sum32()
 	}
 	return
 }
 
-func handleConnection(conn net.Conn, filename string) {
+func handleConnection(conn net.Conn, filename string, hashes []uint32) (err error) {
 	defer conn.Close()
 	r := bufio.NewReader(conn)
 	file, err := os.Open(filename)
 	if err != nil {
-		exit("Failed to open file")
+		return
 	}
 	defer file.Close()
-	stat, _ := file.Stat()
+	stat, err := file.Stat()
+	if err != nil {
+		return
+	}
 	size := stat.Size()
-	fmt.Fprintln(conn, stat.Name())
-	binary.Write(conn, binary.LittleEndian, size)
-	chunks := (size + chunkSize - 1) / chunkSize
-	hashes := calcHashes(file)
-	binary.Write(conn, binary.LittleEndian, hashes)
-	hashOk := make([]bool, chunks, chunks)
-	if err := binary.Read(r, binary.LittleEndian, hashOk); err != nil {
+	if _, err = fmt.Fprintln(conn, stat.Name()); err != nil {
+		return
+	}
+	if err = binary.Write(conn, binary.LittleEndian, size); err != nil {
+		return
+	}
+	if err = binary.Write(conn, binary.LittleEndian, hashes); err != nil {
+		return
+	}
+	hashOk := make([]bool, len(hashes))
+	if err = binary.Read(r, binary.LittleEndian, hashOk); err != nil {
 		return
 	}
 	for i := range hashOk {
 		if !hashOk[i] {
-			file.Seek(int64(i*chunkSize), 0)
-			if _, err := io.CopyN(conn, file, calcChunkSize(i, size)); err != nil {
+			if _, err = file.Seek(int64(i*chunkSize), 0); err != nil {
+				return
+			}
+			if _, err = io.CopyN(conn, file, calcChunkSize(i, size)); err != nil {
 				return
 			}
 		}
 	}
+	return
 }
 
-func connect(server string) {
+func connect(server string) (err error) {
 	conn, err := net.Dial("tcp", server+port)
 	if err != nil {
-		exit("Failed to connect")
+		return
 	}
 	defer conn.Close()
 	r := bufio.NewReader(conn)
 	filename, err := r.ReadString('\n')
 	if err != nil {
-		exit("Failed to read filename")
+		return
 	}
 	filename = filename[:len(filename)-1]
 	var size int64
-	if err := binary.Read(r, binary.LittleEndian, &size); err != nil {
-		exit("Failed to read file size")
+	if err = binary.Read(r, binary.LittleEndian, &size); err != nil {
+		return
 	}
 	fmt.Printf("%s, size = %d bytes\n", filename, size)
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		exit("Failed to open file")
+		return
 	}
 	defer file.Close()
-	file.Truncate(size)
-	chunks := (size + chunkSize - 1) / chunkSize
-	hashes := make([]uint32, chunks, chunks)
-	hashes2 := calcHashes(file)
-	if err := binary.Read(r, binary.LittleEndian, hashes); err != nil {
-		exit("Failed to read hashes")
+	if err = file.Truncate(size); err != nil {
+		return
 	}
-	hashOk := make([]bool, chunks, chunks)
+	chunks := (size + chunkSize - 1) / chunkSize
+	hashes := make([]uint32, chunks)
+	hashes2, err := calcHashes(file)
+	if err != nil {
+		return
+	}
+	if err = binary.Read(r, binary.LittleEndian, hashes); err != nil {
+		return
+	}
+	hashOk := make([]bool, chunks)
 	for i := range hashes2 {
 		hashOk[i] = hashes[i] == hashes2[i]
 	}
-	binary.Write(conn, binary.LittleEndian, hashOk)
+	if err = binary.Write(conn, binary.LittleEndian, hashOk); err != nil {
+		return
+	}
 	bar := pb.StartNew(int(chunks))
 	for i := range hashOk {
 		if !hashOk[i] {
-			file.Seek(int64(i*chunkSize), 0)
+			if _, err = file.Seek(int64(i*chunkSize), 0); err != nil {
+				return
+			}
 			h := crc32.NewIEEE()
-			if _, err := io.CopyN(file, io.TeeReader(r, h), calcChunkSize(i, size)); err != nil {
-				exit("Failed to read chunk")
+			if _, err = io.CopyN(file, io.TeeReader(r, h), calcChunkSize(i, size)); err != nil {
+				return
 			}
 			if h.Sum32() != hashes[i] {
-				exit("Hash mismatch")
+				return errors.New("Hash mismatch")
 			}
 		}
 		bar.Increment()
 	}
 	bar.Finish()
+	return
 }
